6f02d1909d79476482502fcac01f5d75f11bafb6
Geraldine-Edwards <geraldine.edwards25@gmail.com>
2025-10-20 22:13:39 +0100

    Refactor userSongListensData function to export it and improve sorting logic for song listen counts

--- user-song-data.mjs
+++ user-song-data.mjs
@@ -4,7 +4,7 @@ const displayDiv = document.getElementById("displayData");
 // displayDiv.innerHTML = "<p>Your formatted data here</p>";
 
 
-function userSongListensData(userID) {
+export function userSongListensData(userID) {
     const songEvents = getListenEvents(userID);
 
     // starting with an empty object, use the reduce method to count how many times each song ID was listened to by the user.
@@ -14,8 +14,11 @@ function userSongListensData(userID) {
         count[event.song_id] = (count[event.song_id] || 0) + 1;
         return count;
     }, {});
-    console.log(songCounts);
 
+    // Object.entries(songCounts) returns an array of [0] the song ID and [1] the song count, in an array itself.
+    // sorted [1] will sort the number of listens count in descending order (highest to lowest).
+    const sortedSongCount = Object.entries(songCounts).sort((a, b) => b[1] - a[1]);
+    console.log(sortedSongCount);
+    return sortedSongCount;
 
 }
-userSongListensData(userID);
