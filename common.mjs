0a609fec2a7fc120374d7ba71cd771ddf262e1ac
Geraldine-Edwards <geraldine.edwards25@gmail.com>
2025-10-20 17:17:39 +0100

    Feature: Create user dropdown functionality and ensure it initializes on window load

--- common.mjs
+++ common.mjs
@@ -1,3 +1,11 @@
 import { getUserIDs } from "./data.mjs";
+import { createUserDropdown } from "./user-dropdown.mjs"
 
 export const countUsers = () => getUserIDs().length;
+
+window.onload = function () {
+
+// call the dropdown function
+  createUserDropdown();
+
+};
