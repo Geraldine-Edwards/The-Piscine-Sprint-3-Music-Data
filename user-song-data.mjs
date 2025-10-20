1f8a43a8661193aa17f6fca7e37676f17972cda9
Geraldine-Edwards <geraldine.edwards25@gmail.com>
2025-10-20 20:00:11 +0100

    Feature: Add user song data module and update display section

---
+++ user-song-data.mjs
@@ -0,0 +1,4 @@
+import { getListenEvents, getUserIDs, getSong } from "./data.mjs";
+
+const displayDiv = document.getElementById("displayData");
+displayDiv.innerHTML = "<p>Your formatted data here</p>";
